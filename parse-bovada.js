d598046bff02a68b57be49b588f3e24dd7f84321
Kyle Brady <kbrady54@gmail.com>
2018-12-09 11:09:29 -0500

    work on refresh page using setInterval

--- parse-bovada.js
+++ parse-bovada.js
@@ -1,74 +1,85 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+  //
+  const bovadaUrl =
+    "https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl";
+  const scoresUrl = "http://www.nfl.com/liveupdate/scores/scores.json";
 
-//
-const bovadaUrl = 'https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl'
-const scoresUrl = 'http://www.nfl.com/liveupdate/scores/scores.json'
-
-const getLines = async () => {
-  try {
-    const response = await fetch (bovadaUrl);
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      const gamesArray = jsonResponse[0].events;
-      //console.log(gamesArray.length);
-      //console.log(gamesArray);
-      //console.log(gamesArray[0].displayGroups[0].markets[1].description)
-      let i=0
-      for (i=0; i<gamesArray.length; i++) {
-        const eachGame = gamesArray[i].displayGroups[0].markets
-        const indexOfSpread = eachGame.findIndex(p => p.description == "Point Spread")
-        const awayTeamSpread = gamesArray[i].displayGroups[0].markets[indexOfSpread].outcomes[0];
-        const homeTeamSpread = gamesArray[i].displayGroups[0].markets[indexOfSpread].outcomes[1];
-        //const gameTimeUnix = gamesArray[i].startTime;
-        //const gameTimeHuman = new Date(gameTimeUnix * 1000);
-        //console.log(gameTimeHuman)
-        let tr = $('<tr/>');
-        //tr.append("<td>" + gameTimeHuman + "</td>");
-        tr.append("<td>" + awayTeamSpread.description + "</td>");
-        tr.append("<td>" + awayTeamSpread.price.handicap + "</td>");
-        tr.append("<td>" +"@" + "</td>");
-        tr.append("<td>" + homeTeamSpread.description + "</td>");
-        tr.append("<td>" + homeTeamSpread.price.handicap + "</td>");
-        $('#spread-table').append(tr);
-      };
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-getLines();
-
-const getScores = async () => {
-  try {
-    const response = await fetch (scoresUrl);
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      //console.log(jsonResponse)
-      const scoresArray = Object.values(jsonResponse);
-      //console.log(scoresArray[0].away.abbr);
-      //console.log(jsonResponse.away.abbr)
-      for (i=0; i<scoresArray.length; i++) {
-        const awayTeamName = scoresArray[i].away.abbr;
-        const awayTeamScore = scoresArray[i].away.score.T;
-        //console.log(awayTeamScore);
-        const homeTeamName = scoresArray[i].home.abbr;
-        const homeTeamScore = scoresArray[i].home.score.T;
-        let tr = $('<tr/>');
-        tr.append("<td>" + awayTeamName + "</td>");
-        tr.append("<td>" + awayTeamScore + "</td>");
-        tr.append("<td>" +"@" + "</td>");
-        tr.append("<td>" + homeTeamScore + "</td>");
-        tr.append("<td>" + homeTeamName + "</td>");
-        $('#score-table').append(tr);
+  const getLines = async () => {
+    try {
+      const response = await fetch(bovadaUrl);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        const gamesArray = jsonResponse[0].events;
+        //console.log(gamesArray.length);
+        //console.log(gamesArray);
+        //console.log(gamesArray[0].displayGroups[0].markets[1].description)
+        let i = 0;
+        for (i = 0; i < gamesArray.length; i++) {
+          const eachGame = gamesArray[i].displayGroups[0].markets;
+          const indexOfSpread = eachGame.findIndex(
+            p => p.description == "Point Spread"
+          );
+          const awayTeamSpread =
+            gamesArray[i].displayGroups[0].markets[indexOfSpread].outcomes[0];
+          const homeTeamSpread =
+            gamesArray[i].displayGroups[0].markets[indexOfSpread].outcomes[1];
+          //const gameTimeUnix = gamesArray[i].startTime;
+          //const gameTimeHuman = new Date(gameTimeUnix * 1000);
+          //console.log(gameTimeHuman)
+          let tr = $("<tr/>");
+          //tr.append("<td>" + gameTimeHuman + "</td>");
+          tr.append("<td>" + awayTeamSpread.description + "</td>");
+          tr.append("<td>" + awayTeamSpread.price.handicap + "</td>");
+          tr.append("<td>" + "@" + "</td>");
+          tr.append("<td>" + homeTeamSpread.description + "</td>");
+          tr.append("<td>" + homeTeamSpread.price.handicap + "</td>");
+          $("#spread-table").append(tr);
+        }
       }
-  }} catch (error) {
-    console.log(error);
-  }
-};
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-getScores();
+  //getLines();
 
+  const getScores = async () => {
+    try {
+      const response = await fetch(scoresUrl);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        //console.log(jsonResponse)
+        const scoresArray = Object.values(jsonResponse);
+        //console.log(scoresArray[0].away.abbr);
+        //console.log(jsonResponse.away.abbr)
+        for (i = 0; i < scoresArray.length; i++) {
+          const awayTeamName = scoresArray[i].away.abbr;
+          const awayTeamScore = scoresArray[i].away.score.T;
+          //console.log(awayTeamScore);
+          const homeTeamName = scoresArray[i].home.abbr;
+          const homeTeamScore = scoresArray[i].home.score.T;
+          let tr = $("<tr/>");
+          tr.append("<td>" + awayTeamName + "</td>");
+          tr.append("<td>" + awayTeamScore + "</td>");
+          tr.append("<td>" + "@" + "</td>");
+          tr.append("<td>" + homeTeamScore + "</td>");
+          tr.append("<td>" + homeTeamName + "</td>");
+          $("#score-table").append(tr);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getScores();
+
+  setTimeout(function() {
+    window.location.reload(1);
+  }, 5000);
+
+  //let linesTimes = setInterval(getLines, 60000);
+  //let scoresTimer = setInterval(getScores, 5000);
 });
 
 //FANTASTIC GARBAGE CODE!!!!
@@ -97,4 +108,11 @@ function (data) {
         tr.append("<td>" + data[i].team + "</td>");
         $('table').append(tr);
     }
-});*/
+});
+
+for (r = 1; r < scoresArray.length; r++) {
+  let table = document.getElementById("#score-table");
+  table.deleteRow(r);
+}
+
+*/
